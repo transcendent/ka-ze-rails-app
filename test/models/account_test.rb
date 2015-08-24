5673f873666cd2071974a6b60b235e322d5254cb
Tim Hopkins <thopkins@centerstance.com>
2015-08-24 11:11:16 -0400

    Added license and other clean up

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,3 +1,20 @@
+# Copyright 2015 Deloitte. Inc
+# 
+# This file is part of Ka-Ze-Rails-App.
+#
+# Ka-Ze-Rails-App is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# Ka-Ze-Rails-App is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ka-Ze-Rails-App.  If not, see https://github.com/transcendent/ka-ze-rails-app/blob/master/LICENSE
+
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
